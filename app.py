2d5866177bac74354611f6297cd3d3bdb9ad4913
VhastyA <frimpongvhasty@gmail.com>
2026-07-09 12:23:31 -0700

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ def home():
         {"name": "Instagram", "url": "https://www.instagram.com/b_render1?igsh=Nmk3MTczYzM1aXFi"},
         {"name": "TikTok", "url": "https://www.tiktok.com/@b.render5?_r=1&_t=ZS-97hXyN9foQM"},
         {"name": "YouTube", "url": "https://youtube.com/@1b_render?si=MlIqLXb7qqnMnhiV"},
+        {"name": "WhatsApp", "url": "https://wa.me/233552638169"},
         {"name": "LinkedIn", "url": "https://www.linkedin.com/in/emmanuel-gyarko-a0280a2a7?utm_source=share_via&utm_content=profile&utm_medium=member_android"},
         {"name": "Facebook Page", "url": "https://www.facebook.com/share/1BnoYYRRN9/"},
         {"name": "Facebook Profile", "url": "https://www.facebook.com/share/1JAkCYqSNt/"}
@@ -20,4 +21,4 @@ def home():
 
 if __name__ == '__main__':
     app.run(debug=True)
-055 2638169
+
